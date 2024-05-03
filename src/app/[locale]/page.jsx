537b882179e02e40ada956f726464b2f94ac2f96
Khasan-ali <toxtayevhasanali@gmail.com>
2024-05-03 06:49:13 -0400

    delete swiper

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl";
-import Hero from '../../modules/Admin/Home/Hero/page'
 
 
 export default function Home() {
@@ -11,7 +10,7 @@ export default function Home() {
   <div className="container">
     {/* <h1>{t('title')}</h1>
     <p>{t('description')}</p> */}
-    <Hero />
+
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore sequi consequuntur optio ducimus tenetur animi, perspiciatis vel corporis similique, eos provident unde maiores obcaecati, aut laudantium eveniet hic commodi. Itaque?</p>
   </div>
 </div>
